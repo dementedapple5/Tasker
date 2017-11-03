6a9b623658cbd9988409fb4251892a0eca3356e9
Samuel <samuelantequeraworker@gmail.com>
2017-11-03 10:15:23 +0100

    Permisos añadidos

--- app/src/main/java/com/example/usuario/tasker/remote/SOService.java
+++ app/src/main/java/com/example/usuario/tasker/remote/SOService.java
@@ -1,12 +1,10 @@
 package com.example.usuario.tasker.remote;
 
-import okhttp3.ResponseBody;
+
 import retrofit2.Call;
-import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
-import retrofit2.http.Query;
 
 /**
  * Created by Samuel on 02/11/2017.
@@ -19,17 +17,8 @@ public interface SOService {
     @POST("check_user.php")
     Call<Boolean> userExists(@Part("username") String username, @Part("pass") String pass);
 
-/*    @GET("getresiscercanas.php")
-    Call<List<Residencias» getResisCercanas(@Query("lat") String lat, @Query("lng") String lng);
-
-    @GET("getfavresis.php")
-    Call<List<Residencias» getFavResis(@Query("username") String username);
-
     @Multipart
-    @POST("check-resi-favorita.php")
-    Call<ResponseBody> checkResiFavorita(@Part("nombreResi") String nombreResi);
+    @POST("create_user.php")
+    Call<Boolean> createUser(@Part("username") String username, @Part("pass") String pass, @Part("name") String name);
 
-    @Multipart
-    @POST("update-resi-favorita.php")
-    Call<Void> updateResiFavorita(@Part("username") String username, @Part("name") String accion);*/
 }
