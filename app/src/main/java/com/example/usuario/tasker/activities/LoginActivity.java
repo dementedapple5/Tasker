f2c175b5f85ceab7ffa84cdd380c4d6d013add39
Ethan <ethancrespocdm@gmail.com>
2017-11-17 11:20:06 +0100

    server key

--- app/src/main/java/com/example/usuario/tasker/activities/LoginActivity.java
+++ app/src/main/java/com/example/usuario/tasker/activities/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.example.usuario.tasker.activities;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +19,10 @@ import com.example.usuario.tasker.R;
 import com.example.usuario.tasker.remote.ApiUtils;
 import com.example.usuario.tasker.remote.SOService;
 import com.example.usuario.tasker.utilities.Utilities;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
 import java.io.IOException;
 
@@ -116,5 +123,60 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
+
+    public static class MyFirebaseMessagingService extends FirebaseMessagingService {
+        String title, text;
+
+        @Override
+        public void onMessageReceived(RemoteMessage remoteMessage) {
+            // TODO: Handle FCM messages here.
+            // If the application is in the foreground handle both data and notification messages here.
+            // Also if you intend on generating your own notifications as a result of a received FCM
+            // message, here is where that should be initiated.
+            title = remoteMessage.getNotification().getTitle();
+            text = remoteMessage.getNotification().getBody();
+            android.support.v4.app.NotificationCompat.Builder mBuilder;
+            NotificationManager mNotifyMgr = (NotificationManager) getApplicationContext().getSystemService(NOTIFICATION_SERVICE);
+            int icono = R.mipmap.ic_launcher;
+            Intent i = new Intent(this, LoginActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, 0);
+            mBuilder = new NotificationCompat.Builder(getApplicationContext())
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(icono)
+                    .setContentTitle(title)
+                    .setContentText(text)
+                    .setVibrate(new long[]{100, 250, 100, 500})
+                    .setAutoCancel(true);
+            mNotifyMgr.notify(1, mBuilder.build());
+        }
+    }
+
+    public class FirebaseIDService extends FirebaseInstanceIdService {
+
+        private static final String TAG = "FirebaseIDService";
+
+        @Override
+        public void onTokenRefresh() {
+            // Get updated InstanceID token.
+            String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+            // TODO: Implement this method to send any registration to your app's servers.
+            sendRegistrationToServer(refreshedToken);
+        }
+    }
+
+    public void sendRegistrationToServer(String refreshedToken){
+        //insertar bd interna o server o ambas
+
+    }
+
+
+    /*
+    Server Key
+    AAAAyYHqE8k:APA91bF9mmd3oYy4mnoAgebS_mlod6Qyw6munRYLj0DGkEg5faZ8JOml0gwqMDIbc50s0hGj8Pelfo654ACZD69TFrEJ5SZea-xlW-bRU4651OoZK6JReACP8FRWnwWOm_yAGmc7p1Wz
+    *
+    *
+    *
+    *
+    * */
 
 }
